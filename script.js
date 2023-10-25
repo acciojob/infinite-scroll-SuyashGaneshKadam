e60cf25005ab7a49a63521b3bb2f57b0ba93f43a
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-25 15:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
-
+let ol = document.getElementById("infi-list");
+let currentItem = 1;
+function addItems(){
+	for(let i=0 ; i<10 ; i++){
+		let li = document.createElement("li");
+		li.innerText = `Item ${currentItem++}`;
+		ol.append(li);
+	}
+}
+addItems();
+ol.addEventListener("scroll", onScroll);
+function addTwoItems(){
+	for(let i=0 ; i<2 ; i++){
+		let li = document.createElement("li");
+		li.innerText = `Item ${currentItem++}`;
+		ol.append(li);
+	}
+}
+function onScroll(){
+	let totalHeight = ol.scrollHeight;
+	let scrollHeight = ol.clientHeight;
+	let topHeight = ol.scrollTop;
+	let below = totalHeight - scrollHeight - topHeight;
+	if(below < 1) addTwoItems();
+}
